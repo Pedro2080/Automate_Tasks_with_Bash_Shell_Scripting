1c40a6c2ca937194327235928fa74bc537b67d4e
Joao Pedro da Silva <joaopedro2080@hotmail.com>
2022-12-15 22:18:27 +0100

    Added Exercise_01

--- Exercise_01.sh
+++ Exercise_01.sh
@@ -5,6 +5,9 @@ Write a script that prints only the docker status and the docker version.
 mycom
 
 docker_status=$(systemctl status docker | awk '/Active/ { print $3 }' | tr -d "[()]")
+
 docker_version=$(docker -v | awk '/version/ { print  $3 }' | tr -d ",")
+
 echo "The Docker Status is: $docker_status"
+
 echo "The Docker version is: $docker_version"
